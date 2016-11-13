8cc888fd8196559a240de1457b4fb0464dec4e24
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2016-11-13 15:41:24 +0100

    init

---
+++ apihelper.js
@@ -0,0 +1,78 @@
+const pogobuf = require('./pogobuf/pogobuf/pogobuf');
+const logger = require('winston');
+
+function APIHelper(state) {
+    this.state = state;
+}
+
+APIHelper.prototype.always = function(batch) {
+    return batch.checkChallenge()
+                .getHatchedEggs()
+                .getInventory(this.state.api.inventory_timestamp)
+                .checkAwardedBadges()
+                .downloadSettings(this.state.api.settings_hash);
+}
+
+APIHelper.prototype.parse = function(responses) {
+    responses.forEach(r => {
+        if (r.player_data) {
+            // getPlayer()
+            this.state.player = r.player_data;
+            this.state.player.banned = r.banned;
+            this.state.player.warn = r.warn;
+        } else if (r.egg_km_walked) {
+            // getHatchedEggs()
+            console.dir(r, { depth: 4 });
+            for(var stardust in r.stardust_awarded) {
+                //this.state.inventory.player.
+            }
+            for (var xp in r.experience_awarded) {
+                this.state.inventory.player.experience += xp;
+            }
+            for (var candy in r.candy_awarded) {
+                
+            }
+        } else if (r.inventory_delta) {
+            // getInventory()
+            this.state.api.inventory_timestamp = r.inventory_delta.new_timestamp_ms;
+            if (!this.state.hasOwnProperty("inventory")) {
+                this.state.inventory = pogobuf.Utils.splitInventory(r);
+            } else if (r.inventory_delta.inventory_items.length > 0) {
+                console.log("---");
+                console.dir(r.inventory_delta, { depth: 4 });
+                var inventory = pogobuf.Utils.splitInventory(r);
+                console.dir(inventory, { depth: 4 });
+                console.log("---");
+            }
+            // inventory = pogobuf.Utils.splitInventory(r);
+            // for (var k in inventory) {
+            //     if (!this.state.inventory.hasOwnProperty(k)) {
+            //         this.state.inventory[k] = inventory[k];
+            //     } else {
+
+            //     }
+            // }
+            // this.state.inventory = inventory;
+            // console.dir(inventory, { depth: 4 });
+        } else if (r.awarded_badges) {
+            // checkAwardedBadges()
+            console.dir(r, { depth: 4 });
+        } else if (r.hash) {
+            // downloadSettings()
+            this.state.api.settings_hash = r.hash;
+        } else if (r.item_templates_timestamp_ms) {
+            // downloadRemoteConfigVersion()
+           this.state.api.item_templates_timestamp = r.item_templates_timestamp_ms;
+        } else if (r.hasOwnProperty("show_challenge")) {
+            // checkChallenge()
+            if (r.show_challenge) logger.error("Challenge!", { challenge_url: r.challenge_url });
+        } else if (r.hasOwnProperty("digest")) {
+            // getAssetDigest()
+        } else {
+            logger.warn("unhandled");
+            logger.warn(r);
+        }
+    });
+}
+
+module.exports = APIHelper;
---
+++ app.js
@@ -0,0 +1,101 @@
+const pogobuf       = require('./pogobuf/pogobuf/pogobuf');
+const POGOProtos    = require('node-pogo-protos');
+const EventEmitter  = require('events');
+const logger        = require('winston');
+const fs            = require("fs");
+
+const APIHelper = require("./apihelper");
+
+logger.level = "debug";
+
+// blablabekkk0,so0hz835
+// blablabekkk2,gc1gds4h
+
+var config = {
+    credentials: {
+        user: "blablabekkk0",
+        password: "so0hz835"
+    }
+};
+
+var state = {
+    pos: {
+        lat: 48.8456222,
+        lng: 2.3364526
+    },
+    player: {},
+    api: {
+        version: "4500"
+    }
+};
+
+class AppEvents extends EventEmitter {}
+const App = new AppEvents();
+
+var apihelper = new APIHelper(state);
+
+var login = new pogobuf.PTCLogin();
+var client = new pogobuf.Client();
+client.setSignatureInfos({
+    device_info: new POGOProtos.Networking.Envelopes.Signature.DeviceInfo({
+        device_id: "3d65919ca1c2ec3a8e2bd7cc3f975c34",
+        device_brand: "Apple",
+        device_model: "iPhone",
+        device_model_boot: "iPhone8,2",
+        hardware_manufacturer: "Apple",
+        hardware_model: "N66AP",
+        firmware_brand: "iPhone OS",
+        firmware_type: "9.3.5"
+    })
+});
+
+logger.info("App starting...");
+
+// logRequests = function(obj) {
+//     if (obj.hasOwnProperty("requests")) {
+//         var req = obj.requests.filter(r => r.name != "");
+//         req.forEach(logger.debug);
+//     } else if (obj.hasOwnProperty("responses")) {
+//         var res = obj.responses.filter(r => r.name != "Get Asset Digest" && r.name != "Get Item Templates");
+//         res.forEach(logger.debug);
+//     }
+// }
+
+login.login(config.credentials.user, config.credentials.password).then(token => {
+    client.setAuthInfo('ptc', token);
+    client.setPosition(state.pos.lat, state.pos.lng);
+
+    // client.on('request', logRequests);
+    // client.on('response', logRequests);
+
+    return client.init();
+}).then(responses => {
+    apihelper.parse(responses);
+
+    logger.info("Logged In");
+    
+    // download config version like the real app
+    var batch = client.batchStart();
+    batch.downloadRemoteConfigVersion("IOS", state.api.version);
+    apihelper.always(batch);
+    return batch.batchCall();
+}).then(responses => {
+    apihelper.parse(responses);
+
+    var batch = client.batchStart();
+    batch.getAssetDigest(POGOProtos.Enums.Platform.IOS, "Apple", "iPhone", "en", +state.api.version)
+    apihelper.always(batch);
+    return batch.batchCall();
+}).then(responses => {
+    apihelper.parse(responses);
+
+    // check if item_templates need download
+
+    App.emit("apiReady");
+});
+
+App.on("apiReady", () => {
+    logger.info("App ready");
+
+    fs.writeFile("data/state.json", JSON.stringify(state), (err) => {});
+});
