ed8c4c788017853185dbc385dd6ff0f196d6a6a3
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2017-02-14 23:35:28 +0100

    renaming

--- src/captcha/launch.ts
+++ src/captcha/launch.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 
 import CaptchaHelper from './captcha.helper';
 
-let config = require('./helpers/config').load();
+let config = require('../helpers/config').load();
 let state = JSON.parse(fs.readFileSync('data/state.json', 'utf8'));
 
 let url = 'https://pgorelease.nianticlabs.com/plfe/191/captcha/E83DBA71F8C4DBC68A62FBF208C9B046';
