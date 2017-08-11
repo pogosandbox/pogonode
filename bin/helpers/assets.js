a0d401b96f48b827979a6b73aeec39a946d5dc3c
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2017-08-11 16:51:05 +0200

    fix common for translation download

--- bin/helpers/assets.js
+++ bin/helpers/assets.js
@@ -21,7 +21,7 @@ class Assets {
     }
     async loadFromDisk() {
         if (fs.existsSync('data/assets.json')) {
-            let content = await fs.readFile('data/assets.json', 'utf8');
+            const content = await fs.readFile('data/assets.json', 'utf8');
             this.cache = JSON.parse(content);
         }
     }
@@ -29,48 +29,48 @@ class Assets {
         await fs.writeFile('data/assets.json', JSON.stringify(this.cache, null, 4), 'utf8');
     }
     withoutVersion(id) {
-        let idx = id.indexOf('/');
+        const idx = id.indexOf('/');
         if (idx > 0)
             id = id.substring(0, idx);
         return id;
     }
     getFullIdFromId(id) {
-        let digest = this.state.api.asset_digest;
-        let asset = _.find(digest, d => d.asset_id.startsWith(id));
+        const digest = this.state.api.asset_digest;
+        const asset = _.find(digest, d => d.asset_id.startsWith(id));
         return asset.asset_id;
     }
     /**
      * If necessary, get download urls of tranlation assets
      */
     async getTranslationUrls() {
-        let digest = this.state.api.asset_digest;
-        let assets = [];
+        const digest = this.state.api.asset_digest;
+        const assets = [];
         let cached;
-        let general = _.find(digest, d => d.bundle_name === 'i18n_general');
+        const general = _.find(digest, d => d.bundle_name === 'i18n_general');
         cached = this.cache[this.withoutVersion(general.asset_id)];
-        if (!cached || cached !== general.version)
+        if (!cached || +cached !== general.version)
             assets.push(general.asset_id);
-        let modes = _.find(digest, d => d.bundle_name === 'i18n_moves');
+        const modes = _.find(digest, d => d.bundle_name === 'i18n_moves');
         cached = this.cache[this.withoutVersion(modes.asset_id)];
-        if (!cached || cached !== modes.version)
+        if (!cached || +cached !== modes.version)
             assets.push(modes.asset_id);
-        let items = _.find(digest, d => d.bundle_name === 'i18n_items');
+        const items = _.find(digest, d => d.bundle_name === 'i18n_items');
         cached = this.cache[this.withoutVersion(items.asset_id)];
-        if (!cached || cached !== items.version)
+        if (!cached || +cached !== items.version)
             assets.push(items.asset_id);
         if (assets.length > 0) {
             logger.debug('Get translation urls...');
-            await this.downloadAssets(assets, { nobuddy: true, noinbox: true });
+            await this.downloadAssets(assets, { settings: true, nobuddy: true, noinbox: true });
         }
     }
     async getAssetsForPokemons() {
-        let digest = this.state.api.asset_digest;
+        const digest = this.state.api.asset_digest;
         let pokemons = this.state.map.catchable_pokemons.map(p => p.pokemon_id);
         pokemons = _.uniq(pokemons);
-        let assets = [];
-        for (let pokemon of pokemons) {
-            let asset = _.find(digest, d => d.bundle_name.startsWith('pm' + _.padStart(pokemon.toString(), 4, '0')));
-            let cached = this.cache[this.withoutVersion(asset.asset_id)];
+        const assets = [];
+        for (const pokemon of pokemons) {
+            const asset = _.find(digest, d => d.bundle_name.startsWith('pm' + _.padStart(pokemon.toString(), 4, '0')));
+            const cached = this.cache[this.withoutVersion(asset.asset_id)];
             if (!cached || +cached !== asset.version) {
                 assets.push(asset.asset_id);
             }
@@ -82,13 +82,13 @@ class Assets {
     }
     async downloadAssets(assets, commons = {}) {
         if (assets.length > 0) {
-            let client = this.state.client;
-            let batch = client.batchStart();
+            const client = this.state.client;
+            const batch = client.batchStart();
             batch.getDownloadURLs(assets);
             this.apihelper.always(batch, commons);
-            let response = await batch.batchCall();
-            let info = this.apihelper.parse(response);
-            let urls = info.download_urls;
+            const response = await batch.batchCall();
+            const info = this.apihelper.parse(response);
+            const urls = info.download_urls;
             _.each(urls, url => {
                 this.cache[this.withoutVersion(url.asset_id)] = url.asset_id.substring(url.asset_id.indexOf('/') + 1);
             });
