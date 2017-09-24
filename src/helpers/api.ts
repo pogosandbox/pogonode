41180deb6b6ba92c90262e947fbe0a56a1d5b11e
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2017-09-24 19:26:10 +0200

    merge from dev

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,4 +1,4 @@
-import * as pogobuf from '../../pogobuf';
+import * as pogobuf from 'pogobuf-vnext';
 import * as POGOProtos from 'node-pogo-protos-vnext';
 import * as logger from 'winston';
 import * as _ from 'lodash';
