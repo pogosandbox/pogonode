a09f0b2005c8faabb583e735be060e1120d6abd7
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2017-12-09 15:07:17 +0100

    update vscode tasks and tslint

--- bin/debug.js
+++ bin/debug.js
@@ -5,41 +5,39 @@ const fs = require("mz/fs");
 const moment = require("moment");
 const POGOProtos = require("node-pogo-protos-vnext");
 const long = require("long");
+const pogobuf = require("pogobuf-vnext");
 const api_1 = require("./helpers/api");
 const walker_1 = require("./helpers/walker");
-const pogobuf = require("../pogobuf");
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
-    'timestamp': function () {
-        return moment().format('HH:mm:ss');
-    },
+    'timestamp': () => moment().format('HH:mm:ss'),
     'colorize': true,
     'level': 'debug',
 });
-let config = require('./helpers/config').load();
-let state = JSON.parse(fs.readFileSync('data/state.json', 'utf8'));
-let apihelper = new api_1.default(config, state);
-let walker = new walker_1.default(config, state);
+const config = require('./helpers/config').load();
+const state = JSON.parse(fs.readFileSync('data/state.json', 'utf8'));
+const apihelper = new api_1.default(config, state);
+const walker = new walker_1.default(config, state);
 function testVersion(version) {
     logger.info('Version', version);
     logger.info('Client Version', apihelper.versionToClientVersion(version));
     logger.info('iOS Version', apihelper.versionToiOSVersion(version));
 }
 function testRequestIds() {
-    let client = new pogobuf.Client();
+    const client = new pogobuf.Client();
     for (let i = 0; i < 10; i++) {
-        let id = client.getRequestID();
+        const id = client.getRequestID();
         logger.info('%s', id.toString(16));
     }
 }
 function testUK25() {
-    let uk25 = long.fromString('11fdf018c941ef22', false, 16);
+    const uk25 = long.fromString('11fdf018c941ef22', false, 16);
     console.log(uk25.toString());
 }
 async function testDecode() {
     try {
-        let content = await fs.readFile('error_body.bin');
-        let response = POGOProtos.Networking.Envelopes.ResponseEnvelope.decode(content);
+        const content = await fs.readFile('error_body.bin');
+        const response = POGOProtos.Networking.Envelopes.ResponseEnvelope.decode(content);
     }
     catch (e) {
         console.error(e);
--- src/debug.ts
+++ src/debug.ts
@@ -4,26 +4,23 @@ import * as _ from 'lodash';
 import * as moment from 'moment';
 import * as POGOProtos from 'node-pogo-protos-vnext';
 import * as long from 'long';
+import * as pogobuf from 'pogobuf-vnext';
 
 import APIHelper from './helpers/api';
 import Walker from './helpers/walker';
 
-import * as pogobuf from '../pogobuf';
-
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
-    'timestamp': function() {
-        return moment().format('HH:mm:ss');
-    },
+    'timestamp': () => moment().format('HH:mm:ss'),
     'colorize': true,
     'level': 'debug',
 });
 
-let config = require('./helpers/config').load();
-let state = JSON.parse(fs.readFileSync('data/state.json', 'utf8'));
+const config = require('./helpers/config').load();
+const state = JSON.parse(fs.readFileSync('data/state.json', 'utf8'));
 
-let apihelper = new APIHelper(config, state);
-let walker = new Walker(config, state);
+const apihelper = new APIHelper(config, state);
+const walker = new Walker(config, state);
 
 function testVersion(version) {
     logger.info('Version', version);
@@ -32,22 +29,22 @@ function testVersion(version) {
 }
 
 function testRequestIds() {
-    let client = new pogobuf.Client();
+    const client = new pogobuf.Client();
     for (let i = 0; i < 10; i++) {
-        let id = (<any>client).getRequestID();
+        const id = (<any>client).getRequestID();
         logger.info('%s', id.toString(16));
     }
 }
 
 function testUK25() {
-    let uk25 = long.fromString('11fdf018c941ef22', false, 16);
+    const uk25 = long.fromString('11fdf018c941ef22', false, 16);
     console.log(uk25.toString());
 }
 
 async function testDecode() {
     try {
-        let content = await fs.readFile('error_body.bin');
-        let response = POGOProtos.Networking.Envelopes.ResponseEnvelope.decode(content);
+        const content = await fs.readFile('error_body.bin');
+        const response = POGOProtos.Networking.Envelopes.ResponseEnvelope.decode(content);
     } catch (e) {
         console.error(e);
     }
